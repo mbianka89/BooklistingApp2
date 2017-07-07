54dbeb1b81cd23cdb36f00f225280f802180f662
mbianka89 <matyas.bianka28@gmail.com>
2017-07-07 21:58:14 +0200

    search updated

--- app/src/main/java/com/example/android/booklistingapp/BooklistingActivity.java
+++ app/src/main/java/com/example/android/booklistingapp/BooklistingActivity.java
@@ -1,14 +1,16 @@
 package com.example.android.booklistingapp;
 
 import android.app.LoaderManager;
-import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
 import android.content.Loader;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -18,12 +20,11 @@ import java.util.List;
 
 
 public class BooklistingActivity extends AppCompatActivity
-        implements LoaderCallbacks<List<Booklisting>> {
+        implements LoaderManager.LoaderCallbacks<List<Booklisting>> {
 
     private static final String LOG_TAG = BooklistingActivity.class.getName();
 
-    private static final String BOOKLISTING_JSON =
-            "https://www.googleapis.com/books/v1/volumes?maxResults=30&orderBy=newest&q=android";
+    private static final String BOOKLISTING_JSON = "https://www.googleapis.com/books/v1/volumes?maxResults=30&orderBy=newest&q=";
 
     /**
      * Constant value for the booklist loader ID.
@@ -39,8 +40,9 @@ public class BooklistingActivity extends AppCompatActivity
      * TextView that is displayed when the list is empty
      */
     private TextView mEmptyStateTextView;
-    private String mQuery;
     private ProgressBar loadingIndicator;
+
+    private EditText mQueryEditText;
 
 
     @Override
@@ -50,52 +52,72 @@ public class BooklistingActivity extends AppCompatActivity
 
         // Find a reference to the {@link ListView} in the layout
         ListView booklistingListView = (ListView) findViewById(R.id.list);
+        mQueryEditText = (EditText) findViewById(R.id.search_edit_text);
 
         mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
         booklistingListView.setEmptyView(mEmptyStateTextView);
-
-        loadingIndicator = (ProgressBar)findViewById(R.id.loading_indicator);
-
+        loadingIndicator = (ProgressBar) findViewById(R.id.loading_indicator);
         // Create a new adapter that takes an empty list of books as input
         mAdapter = new BooklistingAdapter(this, new ArrayList<Booklisting>());
-
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         booklistingListView.setAdapter(mAdapter);
 
-        // Get a reference to the ConnectivityManager to check state of network connectivity
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
+        mQueryEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // If there is a network connection, fetch data
+                if (isOnline()) {
+                    // Get a reference to the LoaderManager, in order to interact with loaders.
+                    LoaderManager loaderManager = getLoaderManager();
+
+                    //mQuery = s.toString();
+
+                    Bundle args = new Bundle();
+                    args.putString("QUERY", s.toString());
+
+                    // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+                    // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+                    // because this activity implements the LoaderCallbacks interface).
+                    loaderManager.initLoader(BOOKLISTING_LOADER_ID, args, BooklistingActivity.this);
+                    loaderManager.restartLoader(BOOKLISTING_LOADER_ID, args, BooklistingActivity.this);
+                } else {
+                    // Otherwise, display error
+                    // First, hide loading indicator so error message will be visible
+                    View loadingIndicator = findViewById(R.id.loading_indicator);
+                    loadingIndicator.setVisibility(View.GONE);
+
+                    // Update empty state with no connection error message
+                    mEmptyStateTextView.setText(R.string.no_internet_connection);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
+    public boolean isOnline() {
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         // Get details on the currently active default data network
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-        // If there is a network connection, fetch data
-        if (networkInfo != null && networkInfo.isConnected()) {
-            // Get a reference to the LoaderManager, in order to interact with loaders.
-            LoaderManager loaderManager = getLoaderManager();
-
-            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-            // because this activity implements the LoaderCallbacks interface).
-            loaderManager.initLoader(BOOKLISTING_LOADER_ID, null, this);
-        } else {
-            // Otherwise, display error
-            // First, hide loading indicator so error message will be visible
-            View loadingIndicator = findViewById(R.id.loading_indicator);
-            loadingIndicator.setVisibility(View.GONE);
-
-            // Update empty state with no connection error message
-            mEmptyStateTextView.setText(R.string.no_internet_connection);
-        }
-
+        return (networkInfo != null && networkInfo.isConnected());
     }
 
     @Override
     public Loader<List<Booklisting>> onCreateLoader(int i, Bundle bundle) {
         String requestUrl = "";
 
-        //String mQuery = mQuery (EditText R.id.search_edit_text);
+        String mQuery = bundle.getString("QUERY");
 
         if (mQuery != null && mQuery != "") {
             requestUrl = BOOKLISTING_JSON + mQuery;
--- app/src/main/java/com/example/android/booklistingapp/QueryUtils.java
+++ app/src/main/java/com/example/android/booklistingapp/QueryUtils.java
@@ -22,180 +22,178 @@ import java.util.List;
  */
 
 
+/**
+ * Helper methods related to requesting and receiving booklisting data from Google Books API.
+ */
+public final class QueryUtils {
+
+    /** Tag for the log messages */
+    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
     /**
-     * Helper methods related to requesting and receiving booklisting data from Google Books API.
+     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
+     * This class is only meant to hold static variables and methods, which can be accessed
+     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
      */
-    public final class QueryUtils {
+    private QueryUtils() {
+    }
 
-        /** Tag for the log messages */
-        private static final String LOG_TAG = QueryUtils.class.getSimpleName();
+    /**
+     * Query the Google Books API dataset and return a list of {@link Booklisting} objects.
+     */
+    public static List<Booklisting> fetchBooklistingData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
 
-        /**
-         * Create a private constructor because no one should ever create a {@link QueryUtils} object.
-         * This class is only meant to hold static variables and methods, which can be accessed
-         * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
-         */
-        private QueryUtils() {
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
         }
 
-        /**
-         * Query the Google Books API dataset and return a list of {@link Booklisting} objects.
-         */
-        public static List<Booklisting> fetchBooklistingData(String requestUrl) {
-            // Create URL object
-            URL url = createUrl(requestUrl);
+        // Extract relevant fields from the JSON response and create a list of {@link Booklisting}s
+        List<Booklisting> booklistings = extractFeatureFromJson(jsonResponse);
 
-            // Perform HTTP request to the URL and receive a JSON response back
-            String jsonResponse = null;
-            try {
-                jsonResponse = makeHttpRequest(url);
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Problem making the HTTP request.", e);
-            }
+        // Return the list of {@link Booklisting}s
+        return booklistings;
+    }
 
-            // Extract relevant fields from the JSON response and create a list of {@link Booklisting}s
-            List<Booklisting> booklistings = extractFeatureFromJson(jsonResponse);
-
-            // Return the list of {@link Booklisting}s
-            return booklistings;
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
         }
+        return url;
+    }
 
-        /**
-         * Returns new URL object from the given string URL.
-         */
-        private static URL createUrl(String stringUrl) {
-            URL url = null;
-            try {
-                url = new URL(stringUrl);
-            } catch (MalformedURLException e) {
-                Log.e(LOG_TAG, "Problem building the URL ", e);
-            }
-            return url;
-        }
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
 
-        /**
-         * Make an HTTP request to the given URL and return a String as the response.
-         */
-        private static String makeHttpRequest(URL url) throws IOException {
-            String jsonResponse = "";
-
-            // If the URL is null, then return early.
-            if (url == null) {
-                return jsonResponse;
-            }
-
-            HttpURLConnection urlConnection = null;
-            InputStream inputStream = null;
-            try {
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setReadTimeout(10000 /* milliseconds */);
-                urlConnection.setConnectTimeout(15000 /* milliseconds */);
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // If the request was successful (response code 200),
-                // then read the input stream and parse the response.
-                if (urlConnection.getResponseCode() == 200) {
-                    inputStream = urlConnection.getInputStream();
-                    jsonResponse = readFromStream(inputStream);
-                } else {
-                    Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
-                }
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Problem retrieving the booklist JSON results.", e);
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (inputStream != null) {
-                    // Closing the input stream could throw an IOException, which is why
-                    // the makeHttpRequest(URL url) method signature specifies than an IOException
-                    // could be thrown.
-                    inputStream.close();
-                }
-            }
+        // If the URL is null, then return early.
+        if (url == null) {
             return jsonResponse;
         }
 
-        /**
-         * Convert the {@link InputStream} into a String which contains the
-         * whole JSON response from the server.
-         */
-        private static String readFromStream(InputStream inputStream) throws IOException {
-            StringBuilder output = new StringBuilder();
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the booklist JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
             if (inputStream != null) {
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-                BufferedReader reader = new BufferedReader(inputStreamReader);
-                String line = reader.readLine();
-                while (line != null) {
-                    output.append(line);
-                    line = reader.readLine();
-                }
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
             }
-            return output.toString();
         }
-
-        /**
-         * Return a list of {@link Booklisting} objects that has been built up from
-         * parsing the given JSON response.
-         */
-        private static List<Booklisting> extractFeatureFromJson(String booklistingJSON) {
-            // If the JSON string is empty or null, then return early.
-            if (TextUtils.isEmpty(booklistingJSON)) {
-                return null;
-            }
-
-            // Create an empty ArrayList that we can start adding booklistings to
-            List<Booklisting> booklistings = new ArrayList<>();
-
-            // Try to parse the JSON response string. If there's a problem with the way the JSON
-            // is formatted, a JSONException exception object will be thrown.
-            // Catch the exception so the app doesn't crash, and print the error message to the logs.
-            try {
-
-                // Create a JSONObject from the JSON response string
-                JSONObject baseJsonResponse = new JSONObject(booklistingJSON);
-
-                // Extract the JSONArray associated with the key called "items",
-                // which represents a list of features (or booklists).
-                JSONArray booklistingArray = baseJsonResponse.getJSONArray("items");
-
-                // For each book in the booklistingArray, create an {@link Booklisting} object
-                for (int i = 0; i < booklistingArray.length(); i++) {
-
-                    // Get a single booklisting at position i within the list of booklistings
-                    JSONObject currentBooklisting = booklistingArray.getJSONObject(i);
-
-                    // For a given booklisting, extract the JSONObject associated with the
-                    // key called "items", which represents a list of all items
-                    // for that booklisting.
-                    JSONObject items = currentBooklisting.getJSONObject("volumeInfo");
-
-                    // Extract the value for the key called "author"
-                    String author = items.getString("authors");
-
-                    // Extract the value for the key called "title"
-                    String title = items.getString("title");
-
-                    // Create a new {@link Booklisting} object with the author and title
-                    //  from the JSON response.
-                    Booklisting booklisting = new Booklisting(author, title);
-
-                    // Add the new {@link Booklisting} to the list of booklistings.
-                    booklistings.add(booklisting);
-                }
-
-            } catch (JSONException e) {
-                // If an error is thrown when executing any of the above statements in the "try" block,
-                // catch the exception here, so the app doesn't crash. Print a log message
-                // with the message from the exception.
-                Log.e("QueryUtils", "Problem parsing the booklist JSON results", e);
-            }
-
-            // Return the list of booklist
-            return booklistings;
-        }
-
+        return jsonResponse;
     }
 
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
 
+    /**
+     * Return a list of {@link Booklisting} objects that has been built up from
+     * parsing the given JSON response.
+     */
+    private static List<Booklisting> extractFeatureFromJson(String booklistingJSON) {
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(booklistingJSON)) {
+            return null;
+        }
+
+        // Create an empty ArrayList that we can start adding booklistings to
+        List<Booklisting> booklistings = new ArrayList<>();
+
+        // Try to parse the JSON response string. If there's a problem with the way the JSON
+        // is formatted, a JSONException exception object will be thrown.
+        // Catch the exception so the app doesn't crash, and print the error message to the logs.
+        try {
+
+            // Create a JSONObject from the JSON response string
+            JSONObject baseJsonResponse = new JSONObject(booklistingJSON);
+
+            // Extract the JSONArray associated with the key called "items",
+            // which represents a list of features (or booklists).
+            JSONArray booklistingArray = baseJsonResponse.getJSONArray("items");
+
+            // For each book in the booklistingArray, create an {@link Booklisting} object
+            for (int i = 0; i < booklistingArray.length(); i++) {
+
+                // Get a single booklisting at position i within the list of booklistings
+                JSONObject currentBooklisting = booklistingArray.getJSONObject(i);
+
+                // For a given booklisting, extract the JSONObject associated with the
+                // key called "items", which represents a list of all items
+                // for that booklisting.
+                JSONObject items = currentBooklisting.getJSONObject("volumeInfo");
+
+                // Extract the value for the key called "author"
+                String author = items.getString("authors");
+
+                // Extract the value for the key called "title"
+                String title = items.getString("title");
+
+                // Create a new {@link Booklisting} object with the author and title
+                //  from the JSON response.
+                Booklisting booklisting = new Booklisting(author, title);
+
+                // Add the new {@link Booklisting} to the list of booklistings.
+                booklistings.add(booklisting);
+            }
+
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing the booklist JSON results", e);
+        }
+
+        // Return the list of booklist
+        return booklistings;
+    }
+
+}
